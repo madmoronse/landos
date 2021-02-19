a41c2c250776dc8771f78b67e79aa43568fbe517
madmoronse <smolyaninov.se@yandex.ru>
2021-02-19 16:57:05 +0700

    modal

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "flex";
     dots[slideIndex - 1].className += " active";
 }
+
+
+var modal = document.getElementById('myModal');
+
+
+var btn = document.getElementById("teacher-1");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
