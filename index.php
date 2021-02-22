011704be2a5520ccd6ae6d0ddc6e4cdf2dc02c40
madmoronse <smolyaninov.se@yandex.ru>
2021-02-22 19:50:02 +0700

    add img

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/mobile.css">
-    <link rel="icon" href="image/logo.png" type="image/x-icon">
+    <link rel="icon" href="image/favicon.png" type="image/x-icon">
     <title>Школа дропшиппинга от OUTMAX</title>
 </head>
 <body>
